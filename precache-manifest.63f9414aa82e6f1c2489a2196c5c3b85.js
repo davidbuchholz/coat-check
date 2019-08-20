0744397f6ac54e94011f74380d9324b62d9cda17
David Buchholz <davebuchholz13@gmail.com>
2019-08-20 18:22:39 -0400

    Updates

---
+++ precache-manifest.63f9414aa82e6f1c2489a2196c5c3b85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe57af3bf805be337a15264943f98d79",
+    "url": "/coat-check/index.html"
+  },
+  {
+    "revision": "5c5fe3c04553576770e0",
+    "url": "/coat-check/static/css/main.c5b3508b.chunk.css"
+  },
+  {
+    "revision": "5e5202afaa84438daab3",
+    "url": "/coat-check/static/js/2.e892c3ea.chunk.js"
+  },
+  {
+    "revision": "5c5fe3c04553576770e0",
+    "url": "/coat-check/static/js/main.2d0f9ade.chunk.js"
+  },
+  {
+    "revision": "46815a575cfe862fd5cd",
+    "url": "/coat-check/static/js/runtime~main.e0ffd88f.js"
+  }
+]);
